82402f0b8d05e015a6b044c85619e2a8201129ec
geraldarzy <geraldarzola@gmail.com>
2021-04-05 19:45:15 -0400

    show form when signup button is clicked

--- index.js
+++ index.js
@@ -7,5 +7,35 @@ document.addEventListener('DOMContentLoaded',()=>{
     let signupbtn = document.getElementById("signupbtn")
     signupbtn.addEventListener('click',signmeup)
     signinbtn.addEventListener('click',signmein)
+    
+    function createAccountForm(){
+        let form = document.createElement('form')
+        form.innerHTML=
+        `
+        email: <input type='text' id='email'><br>
+        password: <input type='text' id='password'><br>
+        password confirmation: <input type='text' id='password_confirmation'><br>
+        <input type='submit' value='Create Account'>
+        `
+        return form
+    };
+    
+    function signmeup(){
+        let welcome = document.getElementsByClassName('welcome')[0];
+        //make it so that the form is infront of the welcome and you cannot click the welcome page
+        //you have to exit the form before you can click the welcome page
+        showSignupForm();
+    };
+    
+    function showSignupForm(){
+        let form = createAccountForm();
+        document.body.append(form)
+        form.addEventListener('submit',()=>{
+            console.log('penis')
+        });
+    
+    };
+    
+   
 });
 
