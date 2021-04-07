9ff9a9a42cf6b5f8181fb1d6ed1a31c4407bcb1f
geraldarzy <geraldarzola@gmail.com>
2021-04-07 00:41:37 -0400

    add temp function to clear cookies

--- index.js
+++ index.js
@@ -145,5 +145,16 @@ document.addEventListener('DOMContentLoaded',()=>{
         userform.remove();
         homepage.remove();
     }
+    let clearcookies = document.getElementById('clearcookies');
+    clearcookies.addEventListener('click',deleteAllCookies)
+    function deleteAllCookies() {
+        var cookies = document.cookie.split(";");
     
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        }
+    }
 });
