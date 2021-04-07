32225afb8c1ff55c972236faca3eb9dc3fc2e3c8
geraldarzy <geraldarzola@gmail.com>
2021-04-07 18:46:26 -0400

    add a logged in home page

--- src/showpage.js
+++ src/showpage.js
@@ -4,13 +4,27 @@ class Showpage{
     }
     static logged_in_home(){
         Showpage.clearPage();
+        let div = document.createElement('div');
+        div.classList.add('card');
+        div.classList.add('text-white');
+        div.classList.add('bg-dark');
+        div.innerHTML = 
+        `
+        <img class="card-img blurme" src="https://cdn.discordapp.com/attachments/390970702897086465/829467787789074482/Macklemore-Ryan-Lewis-Thrift-Shop-still-billboard-650-1548-compressed.jpg" height="270 px"width="840 px"alt="Card image">
+        <div class="card-img-overlay">
+            <h1 class="card-title centerInside">Thrifty.</h1> 
+            <div class = 'centerInside'>
+                <button class = 'btn btn-light mx-2' > Shop Mens </button>
+                <button class = 'btn btn-light mx-2' > Shop All </button>
+                <button class = 'btn btn-light mx-2' > Shop Womens </button>
+            </div>
+        </div>
+        `
+        document.body.append(div)
     }
     static clearPage(){
         for(let i = 0;i<(document.body.childElementCount);i++){
-            console.log('childcount before' + document.body.childElementCount)
-            console.log('remove child' + i)
             document.body.children[1].remove(); //always remove index 1 because next element goes to index 1 when removing 1
-            console.log("count after " + document.body.childElementCount)
         }
     }
 }
