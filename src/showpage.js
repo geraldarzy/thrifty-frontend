5d6efe544ce87116c060edd0735b4f8f4edcb0c3
geraldarzy <geraldarzola@gmail.com>
2021-04-07 15:57:18 -0400

    add clearPage method to clear the current dom body

--- src/showpage.js
+++ src/showpage.js
@@ -3,9 +3,14 @@ class Showpage{
         
     }
     static logged_in_home(){
-        let homepage = document.getElementsByClassName('welcome')[0];
-        let userform = document.getElementById('user-form');
-        userform.remove();
-        homepage.remove();
+        Showpage.clearPage();
+    }
+    static clearPage(){
+        for(let i = 0;i<(document.body.childElementCount);i++){
+            console.log('childcount before' + document.body.childElementCount)
+            console.log('remove child' + i)
+            document.body.children[1].remove(); //always remove index 1 because next element goes to index 1 when removing 1
+            console.log("count after " + document.body.childElementCount)
+        }
     }
 }
