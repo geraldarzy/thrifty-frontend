7f9686e3f2d78925ccf0d079fb84a362a5bec049
geraldarzy <geraldarzola@gmail.com>
2021-04-10 15:38:51 -0400

    change deletecookies function into logout method

--- src/modals.js
+++ src/modals.js
@@ -4,8 +4,22 @@ class Modals{
     }
 
     static showSignupForm(){
+        $('#signinupModal').modal('hide');
         let form = createUserForm();
-        document.body.append(form)
+        let div = document.createElement('div');
+        div.classList.add('modal','fade');
+        div.id='signup-form';
+        div.innerHTML=`
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class = "modal-content">
+            <button type="button" class="btn btn-outline-info" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                
+        </div>
+        
+    </div>`
+        div.children[0].children[0].append(form);
+        document.body.append(div);
+        $('#signup-form').modal('show');
         form.addEventListener('submit',(event)=>{
             event.preventDefault();
             let email = event.target.email.value;
@@ -21,7 +35,8 @@ class Modals{
         let form = createUserForm();
         form.children[6].value = 'Sign In'
         form.children[4].remove() //change the form to sign in before displaying it
-        document.body.append(form)
+        let modal = document.getElementById('signinupModal');
+        modal.children[0].children[0].children[1].append(form)
         form.addEventListener('submit',(event)=>{
             event.preventDefault();
             let email = event.target.email.value;
@@ -32,6 +47,77 @@ class Modals{
         });
     };
 
+    static addSignInUpModal = () =>{
+        let div = document.createElement('div')
+        div.classList.add('modal','fade');
+        div.id="signinupModal"
+        div.innerHTML = `
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class = "modal-content">
+                
+                <div class="modal-header">
+                  <h3>Thrifty!</h3>
+                  <button type="button" class="btn btn-outline-info" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-content">
+                    <p class="centerOnlyMe centerText" >Welcome! </p>
+                    <p class="centerOnlyMe centerText">The place to shop from all of your favorite Thrift Stores!</p>
+                    <p class="centerOnlyMe centerText"> To continue shopping, please sign in to your account.</p>
+                    <br>
+                    <div class='centerInside'>
+                      <button type="button" class="btn btn-dark" style="margin: 1rem;" id="signupbtn">Sign Up</button> 
+                      <button type="button" class="btn btn-dark" style="margin: 1rem;" id="signinbtn">Sign In</button>
+                    </div>
+                    <br>
+                    <br>
+                </div>
+            </div>
+            
+        </div>
+        `
+        document.body.append(div)
+
+    }
+
+
+    static addAccountInfoModal = ()=>{
+        let div = document.createElement('div')
+            div.classList.add('modal','fade');
+            div.id="loggedInInfo"
+            div.innerHTML = 
+            `
+            <div class="modal-dialog">
+            <div class = "modal-content">
+                
+                <div class="modal-content">
+                    <div class='row'>
+                        <div class='col-sm'>
+                            <fieldset disabled>
+                                <div class="mb-3">
+                                    <label for="disabledTextiInput" class="form-label"><h5>Name:</h5></label>
+                                    <input type="text" id="disabledTextiInput" class="form-control" placeholder="John Doe">
+                                </div>
+                            <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label"><h5>Email:</h5></label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="${Session.getCookie()['email']}">
+                            </div>
+                            </fieldset>
+                            </div>
+                            <div class='col-sm'></div>
+                        </div>
+                        </div>
+                        <div class="modal-footer">
+                        <button class='btn btn-secondary' id='logMeOutButton'>Log Out</button>
+                        </div>
+                        </div>
+                        
+                        </div>
+                        `
+    //                     <!-- $('#myModal').modal('toggle');
+    // $('#myModal').modal('show');
+    // $('#myModal').modal('hide'); -->
+                        document.body.append(div)
+    }
     static addAddItemToCartModal = ()=>{
         //------------------------- add modal
         let modal = document.createElement('div');
@@ -60,27 +146,24 @@ class Modals{
 function createUserForm(){
     let form = document.createElement('form')
     form.id = 'user-form'
+    form.style='padding:5rem;'
     form.innerHTML=
     `
-    <span id = 'userformEmail'>
-        email: <input type='text' id='email'>
-    </span>
-
-    <br>
-
-    <span id = 'userformPassword'>
-        password: <input type='text' id='password'>
-    </span>
-
-    <br>
-
-    <span id = 'userformConfirm'>
-        password confirmation: <input type='text' id='password_confirmation'>
-    </span>
-
-    <br>
-
-    <input type='submit' value='Create Account'>
+    <div class="form-floating centerInside" id = 'userformEmail'>
+                        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                        <label for="email">Email address</label>
+                    </div>
+                    <div class="form-floating centerInside" id = 'userformPassword'>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <label for="password">Password</label>
+                    </div>
+                    <div class="form-floating centerInside" id = 'userformConfirm'>
+                        <input type="password" class="form-control" id="password_confirm" placeholder="Password Confirm">
+                        <label for="password_confirm">Password Confirm</label>
+                    </div>
+                    <div class = "centerInside">
+                        <input type='submit' value='Create Account' class='btn btn-outline-dark centerMe'>
+                    </div>
     `
     return form
 };
--- src/session.js
+++ src/session.js
@@ -59,4 +59,15 @@ class Session{
             //take care of the error showings
         }).catch(error=>console.log(error))
     }
+
+    static logOut() {
+        let cookies = document.cookie.split(";");
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        }
+        $('#loggedInInfo').modal('hide');
+    }
 }
