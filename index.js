d4c6a4e31a175560b483eac48aea3c6deb9f9818
geraldarzy <geraldarzola@gmail.com>
2021-04-10 14:07:46 -0400

    delete delete cookies function

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     let navLogo = document.getElementById('navLogo')
     navLogo.addEventListener('click', Showpage.logged_in_home)
     //---------------------------------------------------------------------------------------------------
-    let d = document.getElementById('deleteCookies');
-    d.addEventListener('click',deleteAllCookies)
-    function deleteAllCookies() {
-        var cookies = document.cookie.split(";");
-    
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i];
-            var eqPos = cookie.indexOf("=");
-            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-        }
-    }
+
 });
 /*
 EDGE CASES TO TAKE CARE OF
