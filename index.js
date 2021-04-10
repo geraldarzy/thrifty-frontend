787e6e5d02a14bbf97af413c0246890235a8ad83
geraldarzy <geraldarzola@gmail.com>
2021-04-10 14:08:36 -0400

    delete unneccessary fetch call; fetch call was initial fetch call to print if domcontentloaded; it did nothing

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    fetch("http://localhost:3000/users").then(resp=>resp.json()).then(json=>{
-        console.log('domcontentloaded')
-        console.log(json)
-    })
+  
     // ------navbar stuff
     let navAccount = document.getElementById('navAccount')
     navAccount.addEventListener('click', Showpage.account_page)
@@ -11,7 +8,6 @@ document.addEventListener('DOMContentLoaded',()=>{
     let navLogo = document.getElementById('navLogo')
     navLogo.addEventListener('click', Showpage.logged_in_home)
     //---------------------------------------------------------------------------------------------------
-
 });
 /*
 EDGE CASES TO TAKE CARE OF
