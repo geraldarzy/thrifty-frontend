f2cdb118c50d5611e7398c6b2a50958634f0abba
geraldarzy <geraldarzola@gmail.com>
2021-04-11 00:00:27 -0400

    addstatic method getCart to retrieve cart items and info from backend

--- src/cart.js
+++ src/cart.js
@@ -2,6 +2,16 @@ class Cart{
     constructor(){
 
     }
-
+    static getCart = (user_id) =>{
+        //logged in or not get the cart info - SRP
+        //to use this method, if user logged in, pass in user_id from the cookie
+        let cart = [];
+        fetch(`http://localhost:3000/carts/${user_id}`).then(resp=>resp.json).then(json=>{
+            //new Item(name,price,size,color,picture,store,sex,id)
+            for(item in json){
+                debugger;
+            }
+        });
+    }
 
 }
